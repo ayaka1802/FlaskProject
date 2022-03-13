2d072040e591a597f23a7b347290967a45ad6613
Ayaka <85811589+ayaka1802@users.noreply.github.com>
2022-03-13 15:15:51 +0900

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,6 +18,23 @@ def post():
     all_onegai = OnegaiContent.query.all()
     return render_template("index.html", name=name, all_onegai=all_onegai)
 
+@app.route("/add",methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title,body,datetime.now())
+    db_session.add(content)
+    db_session.commit()
+    return index()
+
+
+@app.route("/update",methods=["post"])
+def update():
+    content = OnegaiContent.query.filter_by(id=request.form["update"]).first()
+    content.title = request.form["title"]
+    content.body = request.form["body"]
+    db_session.commit()
+    return index()
 
 if __name__ == "__main__":
     app.run(debug=True)
