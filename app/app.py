66746280ef53f06d0cd1fb29908ee09350ff7fa3
Ayaka <85811589+ayaka1802@users.noreply.github.com>
2022-03-13 13:02:09 +0900

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,23 @@
+
+from flask import Flask,render_template,request
+from models.models import OnegaiContent
+app = Flask(__name__)
+
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    name = request.args.get("name")
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
+
+@app.route("/index",methods=["post"])
+def post():
+    name = request.form["name"]
+    all_onegai = OnegaiContent.query.all()
+    return render_template("index.html", name=name, all_onegai=all_onegai)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
